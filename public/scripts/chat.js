a2173a92adb192467417d1a02fb9d1f6ca33dfb8
Marouane <13183518+marouane-erouhi@users.noreply.github.com>
2023-07-10 13:32:58 -0400

    chat frontend working and mostly setup, other features are dependent on backend work

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,24 +1,61 @@
 var socket = io();
 
-console.log('script loaded')
-
-let form = document.getElementById('form');
-let messageInput = document.getElementById('messageInput');
-let messages = document.getElementById('messages');
+let form = document.getElementById('userInputForm');
+let userChatInput = document.getElementById('userChatInput');
+let messages = document.querySelector('.chat__conversation-board');
 let username = "<%= String(user.username) %>"
 
+const createMessageHTML = (username, msg, reversed) => {
+    const isReversed = reversed ? 'reversed' : ''
+    const template = `
+    <div class="chat__conversation-board__message-container ${isReversed}">
+        <div class="chat__conversation-board__message__person">
+            <div class="chat__conversation-board__message__person__avatar"><img
+                    src="https://randomuser.me/api/portraits/men/32.jpg" alt="Thomas Rogh"></div><span
+                class="chat__conversation-board__message__person__nickname">${username}</span>
+        </div>
+        <div class="chat__conversation-board__message__context">
+            <div class="chat__conversation-board__message__bubble"> <span>${msg}</span></div>
+        </div>
+        <div class="chat__conversation-board__message__options">
+            <button class="btn-icon chat__conversation-board__message__option-button option-item emoji-button">
+                <svg class="feather feather-smile sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="24"
+                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+                    <circle cx="12" cy="12" r="10"></circle>
+                    <path d="M8 14s1.5 2 4 2 4-2 4-2"></path>
+                    <line x1="9" y1="9" x2="9.01" y2="9"></line>
+                    <line x1="15" y1="9" x2="15.01" y2="9"></line>
+                </svg>
+            </button>
+            <button class="btn-icon chat__conversation-board__message__option-button option-item more-button">
+                <svg class="feather feather-more-horizontal sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg"
+                    width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+                    <circle cx="12" cy="12" r="1"></circle>
+                    <circle cx="19" cy="12" r="1"></circle>
+                    <circle cx="5" cy="12" r="1"></circle>
+                </svg>
+            </button>
+        </div>
+    </div>
+    `
+    return template
+}
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    if (messageInput.value) {
-        socket.emit('chat message', { username, msg: messageInput.value }); // send message
-        messageInput.value = ''; // reset chat
+    if (userChatInput.value) {
+        socket.emit('chat message', { username, msg: userChatInput.value }); // send message
+        userChatInput.value = ''; // reset chat
     }
 });
 
 socket.on('chat message', function (res) {
-    let item = document.createElement('li');
-    item.textContent = res.username + ': ' + res.msg;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
+    // if user == me: reversed=true, if any other user: reversed=false   
+    let item = createMessageHTML(res.username, res.msg, false)
+    messages.innerHTML += item
+    messages.scrollTop = messages.scrollHeight;
 });
+
+console.log(document.cookie)
